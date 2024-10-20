6f305c3f938e877a9d4af0bc975622c5c807af08
jakimbox <143909984+jakimbox@users.noreply.github.com>
2024-10-20 13:51:18 -0500

    merging Dev into the main branch (#7)
    * more
    
    * more
    
    * Update README.md
    
    * Update README.md
    
    * adding request for service g
    
    * service a completed
    
    * testing integration of orchestrator and service a
    
    * adding folders
    
    * adding template for other microservices
    
    * testing service a
    
    * forgot to import a dependency
    
    * undoing changes
    
    * setting up parameters
    
    ---------
    
    Co-authored-by: gamberfox <gamberfox@gmail.com>

---
+++ service-a/src/main/java/com/example/controller/SlapController.java
@@ -0,0 +1,41 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class SlapController {
+    @GetMapping("/slap")
+    public Map<String, String> getBesos(
+            @RequestParam(value = "slaps", defaultValue = "0") Long slaps,
+            @RequestParam(value = "slapper", defaultValue = "jon") String slapper
+    ) {
+        Map<String, String> besos = new HashMap<>();
+        if(slaps<=0){
+            besos.put("response","you did not get slapped");
+            return besos;
+        }
+        if(slaps==1){
+            besos.put("response","you got slapped once by "+slapper);
+            return besos;
+        }
+        besos.put("response", "you received "
+                +slaps.toString()
+                +" slaps by "
+                +slapper
+        );
+        return besos;
+    }
+
+    @GetMapping("/test")
+    public Map<String,String> test(){
+        Map<String, String> map = new HashMap<>();
+        map.put("test", "hello, you successfully reached the service a");
+        map.put("author", "this service was developed by Javier Grijalba");
+        return map;
+    }
+}
