7d93eb33b09b69cb009e22895c0013f8b6d26024
gamberfox <gamberfox@gmail.com>
2024-10-22 19:13:52 -0500

    testing service g

--- service/orchestrate.py
+++ service/orchestrate.py
@@ -22,35 +22,36 @@ async def orchestrate():
 
         try:
             respuesta_b = await client.get("http://service-b/test",timeout=1.0)
-            data_b = respuesta_b.json()
+            data_b = respuesta_b.text
         except httpx.RequestError:
             data_b = "The service B is not available"
         try:
             respuesta_c = await client.get("http://service-c/test",timeout=1.0)
-            data_c = respuesta_c.json()
+            data_c = respuesta_c.text
         except httpx.RequestError:
             data_c = "The service C is not available"
         try:
             respuesta_d = await client.get("http://service-d/test",timeout=1.0)
-            data_d = respuesta_d.json()
+            data_d = respuesta_d.text
         except httpx.RequestError:
             data_d = "The service D is not available"
         try:
             respuesta_e = await client.get("http://service-e/test",timeout=1.0)
-            data_e = respuesta_e.json()
+            data_e = respuesta_e.text
         except httpx.RequestError:
             data_e = "The service E is not available"
         try:
             respuesta_f = await client.get("http://service-f/test",timeout=1.0)
-            data_f = respuesta_f.json()
+            data_f = respuesta_f.text
         except httpx.RequestError:
             data_f = "The service F is not available"
         
         try:
             respuesta_g = await client.get("http://service-g/test",timeout=1.0)
-            data_g = respuesta_g.json()
+            data_g = respuesta_g.text
+            # data_g = "work"
         except httpx.RequestError:
-            data_g = "The service G is not available"
+            data_g = "The service G is not here"
 
     return {"response_a": data_a,
             "response_b": data_b,
@@ -61,7 +62,7 @@ async def orchestrate():
             "response_g": data_g
             }
 
-## I will only call the service a, developed< by Javier Grijalba
+## I will only call the service a, developed by Javier Grijalba
 @app.get("/a")
 async def serviceA(
     slaps: int = Query(0, description="slaps parameter for service B"),
@@ -89,3 +90,18 @@ async def serviceB():
             data_b = "The service B is not available"
 
         return {"response_b": data_b}
+
+
+@app.get("/g")
+async def serviceG():
+    async with httpx.AsyncClient() as client:
+        try:
+            ##"service-g" is the name of the service
+            respuesta_g = await client.get("http://service-g/test",timeout=1.0)
+            # data_g = respuesta_g.json()
+            data_g = respuesta_g.text
+        except httpx.RequestError as e:
+            data_g = f"HTTP error occurred: {e}"
+        except httpx.HTTPStatusError:
+            data_g = f"Error response {respuesta_g.status_code} from service G"
+        return {"response_g": data_g}
