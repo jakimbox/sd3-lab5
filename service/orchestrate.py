17f4b51ec19fc254d11850afe8f1f667df6e3d53
trijuank <juan.lopez.quitana@correounivalle.edu.co>
2024-10-23 15:45:42 -0500

    actualizar imagen orquestdaor

--- service/orchestrate.py
+++ service/orchestrate.py
@@ -125,6 +125,20 @@ async def serviceC():
         except httpx.HTTPStatusError:
             data_c = f"Error response {respuesta_c.status_code} from service C"
         return {"response_c": data_c}
+    
+@app.get("/d")
+async def serviceD():
+    async with httpx.AsyncClient() as client:
+        try:
+            ##"service-d" is the name of the service
+            respuesta_d = await client.get("http://service-d/test",timeout=1.0)
+            data_d = respuesta_d.text
+        except httpx.RequestError as e:
+            data_d = f"HTTP error occurred: {e}"
+        except httpx.HTTPStatusError:
+            data_d = f"Error response {respuesta_d.status_code} from service D"
+        return {"response_d": data_d}
+
 
 @app.get("/g")
 async def serviceG():
